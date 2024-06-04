c2663b689b05729ddc9c366876e2c5c2fbd2ca27
Gamal Rivaldi M <gamal.rivaldi@gajicermat.com>
2024-06-04 10:20:00 +0700

    Add CTO section and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/Home/About/page";
 import Service from "@/components/Home/Service/page";
 import Hero from "@/components/Home/Hero.component";
 import Work from "@/components/Home/Work/page";
+import CTA from "@/components/Home/Cta/page";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Service />
       <Work />
+      <CTA />
     </main>
   );
 }
--- src/components/Layout/Footer.component.tsx
+++ src/components/Layout/Footer.component.tsx
@@ -1,6 +1,17 @@
+import Social from "../Home/Social.component";
+
 const Footer = () => {
     return (
-      <footer>Footer</footer>
+      <footer className="bg-secondary py-12">
+        <div className="container mx-auto">
+          <div className="flex flex-col items-center justify-between">
+            <Social containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-4" iconStyles="text-primary dark:text-white/70 text-[20px] hover:text-white dark:hover:text-primary transition-all" />
+            <div className="text-muted-foreground">
+              Copyright &copy; Gamal Rivaldi. All rights reserved.
+            </div>
+          </div>
+        </div>
+      </footer>
     )
   }
   
